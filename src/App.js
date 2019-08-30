a0d69d1be9e6d20f28f36bab835968455a44b4ba
Diego Unanue <diego.unanue@globant.com>
2019-08-30 12:11:56 -0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.scss';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       Hello Sliceline!
     </div>
   );
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./navbar.scss";
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <span className="navbar__logo">
+                slice line 🍕
+            </span>
+        </div>
+    );
+}
+
+export default Navbar;
