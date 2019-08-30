793f8e18e44827bb9178b86382857ea451d58aa8
Diego Unanue <diego.unanue@globant.com>
2019-08-30 12:40:42 -0300

    Add banner

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
+import Banner from './components/banner/Banner';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Banner />
       Hello Sliceline!
     </div>
   );
